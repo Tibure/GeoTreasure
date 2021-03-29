8209fbedd005a52ec48d1490d8fde74f26648539
EmBureau <emilebureau@hotmail.com>
2021-03-29 09:11:39 -0400

    Commit notre géolocalisation

--- AppliGeoTreasure/app/src/main/java/com/example/bonneappligeo/TreasureMapsActivity.java
+++ AppliGeoTreasure/app/src/main/java/com/example/bonneappligeo/TreasureMapsActivity.java
@@ -1,44 +1,37 @@
 package com.example.bonneappligeo;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.CancellationToken;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.OnTokenCanceledListener;
+import com.google.android.gms.tasks.Task;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import static com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY;
 
 
 public class TreasureMapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-/*
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_treasure_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
-*/
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    CancellationToken tokenCancellation;
 
 
     @Override
@@ -51,6 +44,8 @@ public class TreasureMapsActivity extends AppCompatActivity implements OnMapRead
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        addListener();
     }
 
     @Override
@@ -58,15 +53,61 @@ public class TreasureMapsActivity extends AppCompatActivity implements OnMapRead
         // Add a marker in Sydney, Australia,
         // and move the map's camera to the same location.
         LatLng sydney = new LatLng(-33.852, 151.211);
-        LatLng saintGeorges = new LatLng(45.3467755,-72.2796581);
+        LatLng saintGeorges = new LatLng(45.3467755, -72.2796581);
         googleMap.addMarker(new MarkerOptions()
-        .position(saintGeorges)
-        .title("Test saint-georges")
-                            );
+                .position(saintGeorges)
+                .title("Test saint-georges")
+        );
         googleMap.addMarker(new MarkerOptions()
                 .position(sydney)
                 .title("Marker in Sydney"));
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+
+        fusedLocationProviderClient.getCurrentLocation(PRIORITY_HIGH_ACCURACY, 2)
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+
+                        if (location != null) {
+
+                        }
+                    }
+                });
+
     }
+
+    private void getLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        fusedLocationProviderClient.getCurrentLocation(PRIORITY_HIGH_ACCURACY, tokenCancellation).addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+
+                if (location != null) {
+                    location.getAltitude();
+                    location.getLatitude();
+                    location.getLongitude();
+                }
+            }
+        });
+
+
+
+    }
+
+    private void addListener(){
+        tokenCancellation.onCanceledRequested(new OnTokenCanceledListener() {
+            @Override
+            public void onCanceled() {
+                Toast.makeText(getApplicationContext(),"allo",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
 }
+
+
 
