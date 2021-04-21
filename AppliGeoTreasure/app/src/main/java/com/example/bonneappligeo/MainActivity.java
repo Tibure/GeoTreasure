aa522f0119af2940aa1ae3c3900b71b78e914fdc
EmBureau <emilebureau@hotmail.com>
2021-04-21 08:43:50 -0400

    Commit Menu fonctionnel ajout des boutons

--- AppliGeoTreasure/app/src/main/java/com/example/bonneappligeo/MainActivity.java
+++ AppliGeoTreasure/app/src/main/java/com/example/bonneappligeo/MainActivity.java
@@ -9,28 +9,39 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    //LinearLayout getHome_play = findViewById(R.id.linearLayout_main_leaderboards);
+    LinearLayout linearLayoutMainLeaderboard, linearLayoutMainPlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-    super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
-        LinearLayout home_play = findViewById(R.id.linearLayout_main_play);
-        test(home_play);
+
+        initViews();
     }
 
-    private void test(LinearLayout linearLayout) {
-        linearLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    private void initViews() {
+        linearLayoutMainPlay = findViewById(R.id.linearLayout_main_play);
+        linearLayoutMainLeaderboard = findViewById(R.id.linearLayout_main_leaderboards);
+
+        linearLayoutMainLeaderboard.setOnClickListener(this);
+        linearLayoutMainPlay.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.linearLayout_main_play:
                 startActivity(new Intent(MainActivity.this, TreasureMapsActivity.class));
-            }
-        });
+                break;
+            case R.id.linearLayout_main_leaderboards:
+                startActivity(new Intent(MainActivity.this, LeaderboardActivity.class));
+                break;
+        }
     }
-
 
 }
